89c00708cf14759c2d24b35b57ff2d4b2acc91ff
Javier Pruebas <moxiyoshida@gmail.com>
2026-04-29 20:15:17 -0500

    Finalizando pruebas Producto

--- TALLER PRUEBAS UNITARIAS/src/test/java/com/generation/ProductoTest.java
+++ TALLER PRUEBAS UNITARIAS/src/test/java/com/generation/ProductoTest.java
@@ -12,9 +12,7 @@ public class ProductoTest
         Producto producto = new Producto("Empanada Infinita",
                 5000, 10);
 
-        double precio = producto.getPrecio();
-
-        assertEquals(5000, precio);
+        assertEquals(5000, producto.getPrecio());
     }
 
     @Test
@@ -24,9 +22,7 @@ public class ProductoTest
           (estaDisponible() devuelve true).*/
         Producto producto = new Producto("Empanada Feliz",
                 5000, 5);
-
-        boolean disponible = producto.estaDisponible();
-        assertTrue(disponible);
+        assertTrue(producto.estaDisponible());
     }
 
     @Test
@@ -34,9 +30,7 @@ public class ProductoTest
     {
         //Un producto con stock igual a cero no está disponible.
         Producto producto = new Producto("Perro caliente", 7000, 0);
-
-        boolean disponible = producto.estaDisponible();
-        assertFalse(disponible);
+        assertFalse(producto.estaDisponible());
     }
 
     @Test
@@ -44,10 +38,23 @@ public class ProductoTest
     {
         //Después de reducir el stock en una cantidad válida, el stock disminuye correctamente.
         Producto producto = new Producto("Bandeja paisa galactica", 10000, 5);
-
         producto.reducirStock(2);
-        int stock = producto.getStock();
+        assertEquals(3, producto.getStock());
+    }
 
-        assertEquals(2, stock);
+    @Test
+    void reducirStockMasDisponible()
+    {
+        //Intentar reducir el stock más de lo disponible lanza IllegalArgumentException.
+        Producto producto = new Producto("Cafe potente", 3000, 5);
+        assertThrows(IllegalArgumentException.class, () -> producto.reducirStock(10));
+    }
+
+    @Test
+    void productoPrecioNegativo()
+    {
+        //Crear un producto con precio negativo lanza IllegalArgumentException.
+        assertThrows(IllegalArgumentException.class, () ->
+                new Producto("Error arepero", -1500, 10));
     }
 }
